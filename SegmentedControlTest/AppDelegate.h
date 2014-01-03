3e1103870af9c4d889fb38a6a58cc94190285e07
iasandcb <iasandcb@gmail.com>
2014-01-03 13:54:56 +0900

    한글로 쓰자 (되도 않는 영어 따위)
    처음으로 올려 본다.

---
+++ SegmentedControlTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SegmentedControlTest
+//
+//  Created by T on 2014. 1. 3..
+//  Copyright (c) 2014년 T. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SegmentedControlTest/ViewController.m
@@ -0,0 +1,32 @@
+//
+//  ViewController.m
+//  SegmentedControlTest
+//
+//  Created by T on 2014. 1. 3..
+//  Copyright (c) 2014년 T. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+- (IBAction)chooseNumber:(id)sender;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)chooseNumber:(id)sender {
+}
+@end
